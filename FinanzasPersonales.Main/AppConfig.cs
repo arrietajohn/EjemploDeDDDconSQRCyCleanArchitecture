fe07d034ae21a908149cbeddd5869972b746497c
John Carlos Arrieta Arrieta <johncarlosarrietaarrieta@gmail.com>
2024-01-24 01:20:31 -0500

    He separado en un archivo aparte de Programa.cs la logica de configuracion de servicios e inyecciones de dependencia y aplicacion de las migraciones a BD

---
+++ FinanzasPersonales.Main/AppConfig.cs
@@ -0,0 +1,43 @@
+﻿
+using FinanzasPersonales.Application.Contracts.Repositories;
+using FinanzasPersonales.Persistence.Database;
+using FinanzasPersonales.Persistence.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+
+public static class AppConfig
+{
+    public static void Configure(HostBuilderContext hostContext, IServiceCollection services)
+    {
+        // Configuración de la cadena de conexión desde appsettings.json
+        IConfiguration configuration = hostContext.Configuration;
+        services.AddDbContext<EfDatabeseContext>(options =>
+            options.UseSqlServer(configuration.GetConnectionString("ConnectionString")));
+
+        services.AddLogging();
+        services.AddScoped<ICategoryRepository, CategoryRepository>();
+    }
+
+    public static void ApplyMigrations(IServiceProvider serviceProvider)
+    {
+        using (var scope = serviceProvider.CreateScope())
+        {
+            var dbContext = scope.ServiceProvider.GetRequiredService<EfDatabeseContext>();
+            var logger = scope.ServiceProvider.GetRequiredService<ILogger<object>>();
+            try
+            {
+                // Aplica las migraciones pendientes
+                dbContext.Database.Migrate();
+                Console.WriteLine("Migraciones aplicadas correctamente.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error al aplicar migraciones: {ex.Message}");
+            }
+        }
+    }
+}
